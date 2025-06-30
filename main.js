2c35e7513fa172480e216a38044408acb322f190
phanphoun <phanphoun855@gmail.com>
2025-06-30 16:22:36 -0700

    HEllo

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById('onclick').addEventListener('click', function () {
+    alert("Let's Login Now!");
+    
+});
+document.getElementById('clicknow').addEventListener('click', function () {
+    alert(about.html);
+});
+
+
+
+
