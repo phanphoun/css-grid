cbe90f66997ecdd5de246c51bd66ace9bea7e136
phanphoun <phanphoun855@gmail.com>
2025-07-01 15:30:28 -0700

    HEl

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ document.getElementById('onclick').addEventListener('click', function () {
     alert("Let's Login Now!");
     
 });
+
 document.getElementById('clicknow').addEventListener('click', function () {
     alert("Please wait while we are preparing for you!");
 });
+
+
 
 
 
